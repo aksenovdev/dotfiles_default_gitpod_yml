77b14cf05b3e16ac1630771cf22be78904205cc6
Sergei Aksenov <aksenovdev@gmail.com>
2025-02-22 23:14:50 +0700

    dsd

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -eu;
 
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
-for _file in 'gitpod.yml'; do {
+for _file in '.gitpod.yml'; do {
     _file_final="${_file}";
     if test -e "$_source_dir/$_file_final" && test ! -e "$GITPOD_REPO_ROOT/$_file_final"; then {
         cp -f "$_source_dir/$_file" "$GITPOD_REPO_ROOT/$_file_final";
